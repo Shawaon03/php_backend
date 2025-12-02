fcd014fc71193a480c6eafd3ec26fa71a1ed0aef
Shawaon03 <shawaonahmed@gmail.com>
2025-12-02 23:52:27 +0600

    Update db.php

--- db.php
+++ db.php
@@ -2,11 +2,12 @@
 $host = getenv("DB_HOST");
 $user = getenv("DB_USER");
 $pass = getenv("DB_PASS");
-$db   = getenv("DB_NAME");
+$dbname = getenv("DB_NAME");
+$port = getenv("DB_PORT");
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass, $dbname, $port);
+
 if ($conn->connect_error) {
-    http_response_code(500);
-    die(json_encode(["error" => "Database connection failed"]));
+    die("Database connection failed: " . $conn->connect_error);
 }
 ?>
